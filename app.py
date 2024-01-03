7404585c7c2b3a84255582e1393fbccfd1f4b163
FatimaAliakberli <149687920+FatimaAliakberli@users.noreply.github.com>
2024-01-03 22:20:37 +0400

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,19 @@ def get_vectorstore(text_chunks):
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
 # Modify the get_conversation_chain function to accept API keys
 def get_conversation_chain(vectorstore, openai_api_key, huggingfacehub_api_token):
     llm = ChatOpenAI(api_key=openai_api_key)
