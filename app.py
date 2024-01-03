5497150d7c087955aecd5b673b226a6fee689c78
FatimaAliakberli <149687920+FatimaAliakberli@users.noreply.github.com>
2024-01-04 01:03:51 +0400

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+import os 
+os.environ["OPENAI_API_KEY"] = "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"
+
 
 def get_pdf_text(pdf_docs):
     text = ""
